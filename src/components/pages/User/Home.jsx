c7abc1280b3e79c4c4004f7355a9ed85c2cab742
Saravanan-0 <saravananmani259@gmail.com>
2023-11-23 10:42:10 +0530

    new2

--- src/components/pages/User/Home.jsx
+++ src/components/pages/User/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../../Common/navbar/navbar'
 
-import User from '../user/user/user'
+import User from './user/user'
 import Work from './Work/Work'
 import Process from './Process/Process'
 
